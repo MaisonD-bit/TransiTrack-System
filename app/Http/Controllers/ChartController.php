1b03dbbe236da536276f0101f988deb84e6a56df
rawburger <kylebonghanoy32@gmail.com>
2025-07-27 17:47:40 +0800

    starting code

---
+++ app/Http/Controllers/ChartController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChartController extends Controller
+{
+    public function activeBusses() {
+        //Logic to retieve active buses
+
+    }
+}
